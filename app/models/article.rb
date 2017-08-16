5ebde5b4008e3925cf9e76990058a305bbcf4ca9
Francisco Coutinho <francisco.lgpc@gmail.com>
2017-08-16 18:27:55 +0200

    updated the article model for audio attachments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :author
 
-  has_attached_file :audio  ,
-                    :url => "/assets/:class/:id/:attachment/:style.:extension",
-                    :path => ":rails_root/public/assets/:class/:id/:attachment/:style.:extension"
+  mount_uploader :audio, AudioUploader
 end
